74c1115c00725e53d45b777e320a4afe37f36438
farvardin <eforgeot@gmail.com>
2013-06-04 12:49:40 +0200

    updated doc. Renamed index.htm to sample.htm

--- t2t.js
+++ t2t.js
@@ -9,10 +9,11 @@
 	function inlineEscape(s)
 	{
 		return escape(s)
-			.replace(/^- ([^*]+)$/, '<ul><li>$1</li></ul>')
+			//.replace(/^- ([^*]+)$/, '<ul><li>$1</li></ul>')
 			//.replace(/- /, '<ul><li>')
+			//.replace(/\s*- ([^*]+)/, '* $1')
 			//.replace(/^ - ([^*]+)$/, '<ul><li>$1</li></ul>')
-			.replace(/^    - ([^*]+)$/, '<ul><li>$1</li></ul>')
+			//.replace(/^    - ([^*]+)$/, '<ul><li>$1</li></ul>')
 			.replace(/\s*=====([^*]+)=====/, '<h5>$1</h5>')
 			.replace(/\s*====([^*]+)====/, '<h4>$1</h4>')
 			.replace(/\s*===([^*]+)===/, '<h3>$1</h3>')
@@ -32,7 +33,7 @@
 			.replace(/\t([^*]+)/g, '<blockquote>$1</blockquote>')
 			.replace(/\t\t([^*]+)/g, '<blockquote><blockquote>$1</blockquote></blockquote>')
 			.replace(/``` ([^*]+)$/, '<pre>$1</pre>')
-			.replace(/\s*|([^*]+)/, '<table><td>$1</td></table>')
+			//.replace(/\s*|([^*]+)/, '<table><td>$1</td></table>')
 			.replace(/^% ([^*]+)$/, '');
 	}
 
@@ -47,8 +48,9 @@
 		{
 		//	'-':[/\n\- /,'<ul><li>','</li></ul>'],
 		//	'-':[/\- /,'<li>','</li>'],
-			'+':[/\+ /,'<ol><li>','</li></ol>'],
+		//	'+':[/\+ /,'<ol><li>','</li></ol>'],
 		//	'-':[/\s+- /,'<ul><li>','</li></ul>'],
+		//	'-':[/(-|_){20,}/g, '<hr/>',''],
 		//	'1':[/\n[1-9]\d*\.? /,'<ol><li>','</li></ol>'],
 		//	' ':[/\n    /,'<pre><code>','</pre></code>','\n'],
 		//	'>':[/\n> /,'<blockquote>','</blockquote>','\n']
@@ -59,6 +61,7 @@
 				.slice(1)
 				.map(R[3]?escape:inlineEscape)
 				.join(R[3]||'</li>\n<li>')+R[2]:
+				//.join(R[3]||'')+R[2]:
 			//f=='#'?'<h'+(f=b.indexOf(' '))+'>'+inlineEscape(b.slice(f+1))+'</h'+f+'>':
 			//f=='<'?b:
 			'<p>'+inlineEscape(b)+'</p>';
